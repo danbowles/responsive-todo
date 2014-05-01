32fdadbf2dc72dd4e88a3514dd5105f255e5e870
OpenShift guest <daniel.bowles@gmail.com>
2014-04-30 21:05:11 -0400

    Cleanup of server, adding some packages all willy-nilly style

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 // Setup
 var express         = require('express');
-var morgan          = require('morgan');
+var path            = require('path');
+var favicon         = require('static-favicon');
+var logger          = require('morgan');
+var cookieParser    = require('cookie-parser');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
-var app             = express();
 
+// TODO database
+// TODO routes
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
+// TODO - move to 'www' or similar file
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.IP;
 var port    = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT;
 
-app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
-app.use(bodyParser());
-app.use(methodOverride());
-
 app.listen(port, ipaddr);
 console.log('Started. Listening on ' + ipaddr + ':' + port);
+// end TODO
 
