abdc4f122e635653694a5f87cd316cf1532d227a
OpenShift guest <daniel.bowles@gmail.com>
2014-05-22 16:31:02 -0400

    Added connection string for openShift

--- db.js
+++ db.js
@@ -1,6 +1,18 @@
 var mongo = require('mongoskin');
-var db = mongo.db('mongodb://' + process.env.IP + ':27017/todos', 
-  {native_parser: true}
-);
+var connectionString = process.env.IP + ':27017/todos';
+var db;
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+
+
+
+db = mongo.db('mongodb://' + connectionString, {native_parser: true});
 
 module.exports = db;
