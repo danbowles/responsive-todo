d555e120bd905f908c2249847c53ee1af94b4e96
Dan Bowles <daniel.bowles@gmail.com>
2014-07-21 09:49:13 -0400

    Updating www to be a .js file, not bash

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,8 @@
-#!/usr/bin/env node
 var debug = require('debug')('todo');
 var app = require('../app');
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT);
-app.set('address', process.env.OPENSHIFT_NODEJS_IP || process.env.IP);
+app.set('address', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || 3000);
 
 var server = app.listen(app.get('port'), app.get('address'), function() {
   debug('Listening on ' + server.address() + ':' + server.address().port);
